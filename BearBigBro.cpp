ef12c0de6111338a1de1d54c66ed5eb5ffbcf3f6
myfirstrepo <abhaysingh6575@gmail.com>
2025-03-29 14:36:05 +0530

    done

---
+++ BearBigBro.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cin >> a >> b;
+
+    int years = 0;
+
+    while(b >= a){
+        a *= 3;
+        b *= 2;
+        years++;
+    }
+
+    cout << years << endl;
+    return 0;
+}
