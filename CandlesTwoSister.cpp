8711c0be548951178093716a6a4f299963b18f29
myfirstrepo <abhaysingh6575@gmail.com>
2025-05-13 09:34:33 +0530

    done

---
+++ CandlesTwoSister.cpp
@@ -0,0 +1,22 @@
+//BACKUP QUESTION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n <= 2) {
+            cout << 0 << endl;
+        } else {
+            cout << (n - 1) / 2 << endl;
+        }
+    }
+
+    return 0;
+}
