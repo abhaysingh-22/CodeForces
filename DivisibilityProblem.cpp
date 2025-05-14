3d2c40cdfe54aeea9b900cf05078adb6488ebfeb
myfirstrepo <abhaysingh6575@gmail.com>
2025-05-15 03:43:33 +0530

    done

---
+++ DivisibilityProblem.cpp
@@ -0,0 +1,24 @@
+//BACKUP QUESTION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        
+        int a, b;
+        cin >> a >> b;
+
+        if(a % b == 0){
+            cout << 0 << endl;
+        }else{
+            int r = a % b;
+            cout << b - r << endl;
+        }
+    }
+    return 0;
+}
