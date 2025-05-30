2f10f4b2fe624b47edcc1570895ea4064beecb30
myfirstrepo <abhaysingh6575@gmail.com>
2025-05-30 09:19:43 +0530

    done

---
+++ NonCoprime.cpp
@@ -0,0 +1,24 @@
+//BACKUP QUESTION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n==2 || n==1){
+        cout<<"No"<<endl;
+    }else{
+        cout<<"Yes"<<endl;
+
+        cout << 1 << " " << n << endl;
+
+        cout << n - 1 << " ";
+        for(int i = 1; i < n; i++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
