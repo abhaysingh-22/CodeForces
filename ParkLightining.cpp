ca579d656bb1da9ed6986b77a6b92e4ddacc1750
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-06 20:29:54 +0530

    done

---
+++ ParkLightining.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        int n, m;
+        cin >> n >> m;
+
+        int total = n * m;
+        int light = (total + 1) / 2;
+
+        cout << light << endl;
+    }
+
+    return 0;
+}
